4795d31c84f79fdafa7b1cbe3e9ed01455148de7
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-03-10 15:29:36 +0200

    feat-refactor: todo-form, todoFromServer

---
+++ src/app/data/todosFromServer.ts
@@ -0,0 +1,10 @@
+import { Todo } from "../types/todo";
+
+export const todosFromServer: Todo[] = [
+    { id: 1, title: 'HTML', completed: true },
+    { id: 2, title: 'CSS', completed: true },
+    { id: 3, title: 'JS', completed: false },
+    { id: 4, title: 'React', completed: false },
+    { id: 5, title: 'Vue', completed: false },
+    { id: 6, title: 'Angular', completed: false },
+  ];
