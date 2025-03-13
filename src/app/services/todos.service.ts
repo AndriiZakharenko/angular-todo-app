aaaf3e25c4fb07c0139b0ff4613de1fb71268705
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-03-13 23:51:51 +0200

    feat-fetching-data: get fake API

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,20 +1,55 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { API} from '../constants/constants';
+import { API } from '../constants/constants';
 import { Todo } from '../types/todo';
-
-
+import { BehaviorSubject, Observable, switchMap, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodosService {
+  refresh$$ = new BehaviorSubject(null);
+  todos$: Observable<Todo[]>;
 
-  constructor(
-    private http: HttpClient,
-  ) { }
+  constructor(private http: HttpClient) {
+    this.todos$ = this.refresh$$.pipe(switchMap(() => this.getTodos()));
+  }
 
-  getTodos(){
-    return this.http.get<Todo[]>(`${API.url}/todos?userId=${API.userId}`)
+  getTodos() {
+    return this.http.get<Todo[]>(`${API.url}/todos?userId=${API.userId}`);
+  }
+
+  createTodo(title: string) {
+    return this.http
+      .post<Todo>(`${API.url}/todos`, {
+        title,
+        userId: API.userId,
+        completed: false,
+      })
+      .pipe(tap(() => this.refresh$$.next(null)));
+    // .pipe(
+    //   withLatestFrom(this.todos$$),
+    //   tap(([createdTodo, todos]) => {
+    //     this.todos$$.next(
+    //       [...todos, createdTodo]
+    //     );
+    //   }),
+    // )
+  }
+
+  updateTodo(todo: Todo) {
+    return this.http.patch<Todo>(`${API.url}/todos/${todo.id}`, todo).pipe(
+      tap(() => {
+        this.refresh$$.next(null);
+      })
+    );
+  }
+
+  deleteTodo(todo: Todo) {
+    return this.http.delete<Todo>(`${API.url}/todos/${todo.id}`).pipe(
+      tap(() => {
+        this.refresh$$.next(null);
+      })
+    );
   }
 }
