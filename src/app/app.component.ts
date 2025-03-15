c36bda5539b6030d92e00a211a1794a2d81961fe
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-03-15 22:04:19 +0200

    feat-error: error message & service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,19 @@ import { TodoComponent } from './components/todo/todo.component';
 import { Todo } from './types/todo';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { TodosService } from './services/todos.service';
+import { MessageComponent } from './components/message/message.component';
+import { MessageService } from './services/message.service';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, TodoComponent, TodoFormComponent],
+  imports: [CommonModule, TodoComponent, TodoFormComponent, MessageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   private _todos: Todo[] = [];
   activeTodos: Todo[] = [];
+  errorMessage = '';
 
   get todos() {
     return this._todos;
@@ -28,14 +31,21 @@ export class AppComponent implements OnInit {
     this.activeTodos = this._todos.filter((todo) => !todo.completed);
   }
 
-  constructor(private todosServices: TodosService) {}
+  constructor(
+    private todosServices: TodosService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.todosServices.todos$.subscribe((todos) => {
       this.todos = todos;
     });
 
-    this.todosServices.loadTodos().subscribe()
+    this.todosServices.loadTodos().subscribe({
+      error: () => {
+        this.messageService.showMessage('Unable to load todos')
+      },
+    });
   }
 
   trackById(i: number, todo: Todo) {
@@ -43,20 +53,36 @@ export class AppComponent implements OnInit {
   }
 
   addTodo(newTitle: string) {
-    this.todosServices.createTodo(newTitle).subscribe();
+    this.todosServices.createTodo(newTitle).subscribe({
+      error: () => {
+        this.messageService.showMessage('Unable to load todos')
+      },
+    });
   }
 
   toggleTodo(todo: Todo) {
     this.todosServices
       .updateTodo({ ...todo, completed: !todo.completed })
-      .subscribe();
+      .subscribe({
+        error: () => {
+          this.messageService.showMessage('Unable to load todos')
+        },
+      });
   }
 
   renameTodo(todo: Todo, title: string) {
-    this.todosServices.updateTodo({ ...todo, title }).subscribe();
+    this.todosServices.updateTodo({ ...todo, title }).subscribe({
+      error: () => {
+        this.messageService.showMessage('Unable to load todos')
+      },
+    });
   }
 
   deleteTodo(todo: Todo) {
-    this.todosServices.deleteTodo(todo).subscribe();
+    this.todosServices.deleteTodo(todo).subscribe({
+      error: () => {
+        this.messageService.showMessage('Unable to load todos')
+      },
+    });
   }
 }
---
+++ src/app/components/message/message.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MessageService } from '../../services/message.service';
+
+@Component({
+  selector: 'app-message',
+  imports: [],
+  templateUrl: './message.component.html',
+  styleUrl: './message.component.scss',
+})
+export class MessageComponent implements OnInit {
+  @Input() title = 'Error';
+
+  message = '';
+  hidden = true;
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.messageService.message$.subscribe((text) => {
+      this.hidden = false;
+      this.message = text;
+    });
+  }
+}
