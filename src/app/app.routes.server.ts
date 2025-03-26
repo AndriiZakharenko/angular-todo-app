655f464d27d6b41c01938bddc47ad94a56329ad9
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-03-26 23:36:19 +0200

    feat-prerender: deploy failure

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,21 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'todos/:status',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams() {
+      return Promise.resolve([
+        { status: 'all' },
+        { status: 'active' },
+        { status: 'completed' }
+      ]);
+    }
+  },
+  {
+    path: 'about',
+    renderMode: RenderMode.Prerender
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
