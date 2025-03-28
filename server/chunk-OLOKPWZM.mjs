434846ae57990835223308a4948110f26337275f
Andrii Zakharenko <andriizakharenko@gmail.com>
2025-03-29 00:35:50 +0200

    Auto-generated commit

--- server/chunk-OLOKPWZM.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{M as i,N as p,Ua as c,Z as m,_ as r,ha as s,m as a,ob as u}from"./chunk-KSOPU4CO.mjs";import"./chunk-X2SEQXRR.mjs";var o=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=i({type:t,selectors:[["app-about-page"]],decls:2,vars:0,template:function(e,d){e&1&&(m(0,"p"),s(1,"about-page works!"),r())},encapsulation:2})};var f=class t{static \u0275fac=function(e){return new(e||t)};static \u0275mod=p({type:t});static \u0275inj=a({imports:[c,u.forChild([{path:"",component:o}])]})};export{f as AboutModule};
